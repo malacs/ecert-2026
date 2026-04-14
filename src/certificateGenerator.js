315a1cc25db2bfc8981475c9d4d6666ada82802d
malacs <justlanner@gmail.com>
2026-04-14 17:21:54 +0800

    Update certificateGenerator.js

--- src/certificateGenerator.js
+++ src/certificateGenerator.js
@@ -206,7 +206,7 @@ export const generateCertificate = async (participantName, trainingDay = null) =
 
   // ── SIGNATURE (UPDATED BASED ON IMAGE) ─────────────────────────
   ctx.filter = 'brightness(0.6) contrast(1.2)';
-  ctx.drawImage(logoSig, W / 2 - 50, 628, 70, 45); // higher + smaller
+  ctx.drawImage(logoSig, W / 2 - 55, 615, 70, 45); // higher + smaller
   ctx.filter = 'none';
 
   ctx.fillStyle = '#ffffff';
